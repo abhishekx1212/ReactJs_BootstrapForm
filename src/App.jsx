517d1faa65ad696f3c0fa0d1ea28f2ad7a28eae3
abhishekx1212 <trigonx1212@gmail.com>
2024-09-14 18:42:36 +0530

    BootstrapForm

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import FormData from './Components/FormData'
+function App() {
+  return (
+    <>
+      <FormData/>
+    </>
+  )
+}
+
+export default App
